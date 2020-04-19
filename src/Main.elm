92efa682075f9457371ac5d940f11a63c294c733
brandly <brandly13@gmail.com>
2020-04-19 17:09:49 -0400

    blocks and balls example

--- src/Main.elm
+++ src/Main.elm
@@ -1,60 +1,317 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Angle exposing (Angle)
+import Acceleration
+import Angle
+import Array exposing (Array)
+import Block3d
+import Browser
+import Browser.Dom as Dom
+import Browser.Events as Events
 import Camera3d
-import Color
 import Direction3d
+import Duration
+import Frame3d
 import Html exposing (Html)
-import Length
+import Html.Attributes
+import Illuminance
+import Length exposing (Length, inMeters, meters)
+import Luminance
+import Mass
+import Materials
 import Palette.Tango as Tango
-import Pixels
+import Physics.Body as Body exposing (Body)
+import Physics.Coordinates exposing (BodyCoordinates, WorldCoordinates)
+import Physics.World as World exposing (World)
+import Pixels exposing (pixels)
 import Point3d
+import Random
 import Scene3d
 import Scene3d.Material as Material
-import Scene3d.Mesh as Mesh
-import SketchPlane3d
-import Triangle3d
+import Sphere3d
+import Task
 import Viewpoint3d
 
 
-main : Html msg
+type alias Model =
+    { world : World (Scene3d.Entity BodyCoordinates)
+    , screenWidth : Float
+    , screenHeight : Float
+    }
+
+
+type Msg
+    = Tick Float
+    | Resize Float Float
+
+
+main : Program () Model Msg
 main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { world = initialWorld
+      , screenWidth = 0
+      , screenHeight = 0
+      }
+    , Task.perform
+        (\{ viewport } -> Resize viewport.width viewport.height)
+        Dom.getViewport
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick _ ->
+            ( { model | world = World.simulate (Duration.seconds (1 / 60)) model.world }
+            , Cmd.none
+            )
+
+        Resize width height ->
+            ( { model | screenWidth = width, screenHeight = height }
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ Events.onResize (\w h -> Resize (toFloat w) (toFloat h))
+        , Events.onAnimationFrameDelta Tick
+        ]
+
+
+view : Model -> Html Msg
+view { world, screenWidth, screenHeight } =
     let
-        triangle1 =
-            Triangle3d.from
-                (Point3d.meters 0 0 0)
-                (Point3d.meters 1 0 0)
-                (Point3d.meters 1 1 0)
-
-        triangle2 =
-            Triangle3d.from
-                (Point3d.meters 0 0 0)
-                (Point3d.meters 1 1 0)
-                (Point3d.meters 0 1 0)
-
-        mesh1 =
-            Mesh.triangles [ triangle1 ]
-
-        mesh2 =
-            Mesh.triangles [ triangle2 ]
-
         camera =
             Camera3d.perspective
                 { viewpoint =
                     Viewpoint3d.lookAt
-                        { focalPoint = Point3d.meters 0.5 0.5 0
-                        , eyePoint = Point3d.meters 3 3 2
-                        , upDirection = Direction3d.z
+                        { eyePoint = Point3d.meters 0 20 20
+                        , focalPoint = Point3d.meters 0 0 0
+                        , upDirection = Direction3d.positiveZ
                         }
-                , verticalFieldOfView = Angle.degrees 30
+                , verticalFieldOfView = Angle.degrees 24
+                }
+
+        drawables =
+            List.map getTransformedDrawable (World.bodies world)
+
+        sunlight =
+            Scene3d.directionalLight Scene3d.castsShadows
+                { chromaticity = Scene3d.sunlight
+                , intensity = Illuminance.lux 10000
+                , direction = Direction3d.xyZ (Angle.degrees 45) (Angle.degrees -60)
+                }
+
+        daylight =
+            Scene3d.overheadLighting
+                { upDirection = Direction3d.z
+                , chromaticity = Scene3d.daylight
+                , intensity = Illuminance.lux 15000
                 }
     in
-    Scene3d.unlit
-        { camera = camera
-        , clipDepth = Length.meters 0.1
-        , dimensions = ( Pixels.pixels 800, Pixels.pixels 600 )
-        , background = Scene3d.transparentBackground
-        }
-        [ Scene3d.mesh (Material.color Tango.orange2) mesh1
-        , Scene3d.mesh (Material.color Tango.skyBlue2) mesh2
+    Html.div
+        [ Html.Attributes.style "position" "absolute"
+        , Html.Attributes.style "left" "0"
+        , Html.Attributes.style "top" "0"
         ]
+        [ Scene3d.toHtml
+            { dimensions = ( pixels screenWidth, pixels screenHeight )
+            , antialiasing = Scene3d.multisampling
+            , camera = camera
+            , lights = Scene3d.twoLights sunlight daylight
+            , exposure = Scene3d.maxLuminance (Luminance.nits 10000)
+            , toneMapping = Scene3d.noToneMapping
+            , whiteBalance = Scene3d.daylight
+            , background = Scene3d.transparentBackground
+            , clipDepth = meters 0.1
+            }
+            drawables
+        ]
+
+
+initialWorld : World (Scene3d.Entity BodyCoordinates)
+initialWorld =
+    let
+        moonGravity =
+            Acceleration.metersPerSecondSquared 1.62
+    in
+    World.empty
+        |> World.withGravity moonGravity Direction3d.negativeZ
+        |> World.add floor
+        |> addBoxes
+
+
+materials : Array (Material.Uniform coordinates)
+materials =
+    Array.fromList
+        [ Materials.aluminum
+        , Materials.whitePlastic
+        , Materials.copper
+        , Material.nonmetal
+            { baseColor = Tango.skyBlue1
+            , roughness = 0.25
+            }
+        , Materials.gold
+        , Materials.whitePlastic
+        , Material.nonmetal
+            { baseColor = Tango.skyBlue2
+            , roughness = 0.25
+            }
+        ]
+
+
+type alias Offsets =
+    { x : Float
+    , y : Float
+    , z : Float
+    }
+
+
+offsetGenerator : Random.Generator Offsets
+offsetGenerator =
+    let
+        magnitude =
+            0.01
+    in
+    Random.map3 Offsets
+        (Random.float -magnitude magnitude)
+        (Random.float -magnitude magnitude)
+        (Random.float -magnitude magnitude)
+
+
+randomOffsets : Int -> Offsets
+randomOffsets index =
+    Random.step offsetGenerator (Random.initialSeed index)
+        |> Tuple.first
+
+
+addBoxes : World (Scene3d.Entity BodyCoordinates) -> World (Scene3d.Entity BodyCoordinates)
+addBoxes world =
+    let
+        xySize =
+            4
+
+        zSize =
+            5
+
+        xyDimensions =
+            List.map toFloat (List.range 0 (xySize - 1))
+
+        zDimensions =
+            List.map toFloat (List.range 0 (zSize - 1))
+
+        distance =
+            1
+    in
+    List.foldl
+        (\x world1 ->
+            List.foldl
+                (\y world2 ->
+                    List.foldl
+                        (\z ->
+                            let
+                                index =
+                                    round (z * xySize * xySize + y * xySize + x)
+
+                                material =
+                                    Array.get (index |> modBy (Array.length materials)) materials
+                                        |> Maybe.withDefault Materials.aluminum
+
+                                body =
+                                    if (index |> modBy 3) == 0 then
+                                        box material
+
+                                    else
+                                        sphere (Material.uniform material)
+
+                                offsets =
+                                    randomOffsets index
+                            in
+                            body
+                                |> Body.moveTo
+                                    (Point3d.meters
+                                        ((x - (xySize - 1) / 2) * distance + offsets.x)
+                                        ((y - (xySize - 1) / 2) * distance + offsets.y)
+                                        ((z + (2 * zSize + 1) / 2) * distance + offsets.z)
+                                    )
+                                |> World.add
+                        )
+                        world2
+                        zDimensions
+                )
+                world1
+                xyDimensions
+        )
+        world
+        xyDimensions
+
+
+floorRadius : Length
+floorRadius =
+    Length.meters 30
+
+
+floor : Body (Scene3d.Entity BodyCoordinates)
+floor =
+    let
+        shape =
+            Sphere3d.atOrigin floorRadius
+    in
+    Scene3d.sphere Scene3d.doesNotCastShadows
+        (Material.uniform Materials.aluminum)
+        shape
+        |> Body.sphere shape
+        |> Body.moveTo
+            (Point3d.meters
+                0
+                0
+                -(Length.inMeters floorRadius)
+            )
+
+
+boxSize : Length
+boxSize =
+    Length.meters 0.9
+
+
+box : Material.Uniform BodyCoordinates -> Body (Scene3d.Entity BodyCoordinates)
+box material =
+    let
+        shape =
+            Block3d.centeredOn Frame3d.atOrigin
+                ( boxSize, boxSize, boxSize )
+    in
+    Scene3d.block Scene3d.castsShadows material shape
+        |> Body.block shape
+        |> Body.withBehavior (Body.dynamic (Mass.kilograms 5))
+
+
+sphereRadius : Length
+sphereRadius =
+    Length.meters 0.45
+
+
+sphere : Material.Textured BodyCoordinates -> Body (Scene3d.Entity BodyCoordinates)
+sphere material =
+    let
+        shape =
+            Sphere3d.atOrigin sphereRadius
+    in
+    Scene3d.sphere Scene3d.castsShadows material shape
+        |> Body.sphere shape
+        |> Body.withBehavior (Body.dynamic (Mass.kilograms 2.5))
+
+
+getTransformedDrawable : Body (Scene3d.Entity BodyCoordinates) -> Scene3d.Entity WorldCoordinates
+getTransformedDrawable body =
+    Scene3d.placeIn (Body.frame body) (Body.data body)
---
+++ src/Materials.elm
@@ -0,0 +1,59 @@
+module Materials exposing
+    ( aluminum
+    , blackPlastic
+    , chromium
+    , copper
+    , gold
+    , whitePlastic
+    )
+
+import Color exposing (Color)
+import Scene3d.Material as Material exposing (Material)
+
+
+gold : Material.Uniform coordinates
+gold =
+    Material.metal
+        { baseColor = Color.fromRGB ( 255, 195, 86 )
+        , roughness = 0.4
+        }
+
+
+aluminum : Material.Uniform coordinates
+aluminum =
+    Material.metal
+        { baseColor = Color.fromRGB ( 233, 235, 236 )
+        , roughness = 0.6
+        }
+
+
+copper : Material.Uniform coordinates
+copper =
+    Material.metal
+        { baseColor = Color.fromRGB ( 244, 162, 137 )
+        , roughness = 0.25
+        }
+
+
+chromium : Material.Uniform coordinates
+chromium =
+    Material.metal
+        { baseColor = Color.fromRGB ( 140, 142, 141 )
+        , roughness = 0.5
+        }
+
+
+blackPlastic : Material.Uniform coordinates
+blackPlastic =
+    Material.nonmetal
+        { baseColor = Color.fromRGB ( 0, 0, 0 )
+        , roughness = 0.5
+        }
+
+
+whitePlastic : Material.Uniform coordinates
+whitePlastic =
+    Material.nonmetal
+        { baseColor = Color.fromRGB ( 255, 255, 255 )
+        , roughness = 0.25
+        }
